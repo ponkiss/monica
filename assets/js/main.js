cc4c16c343a5b8847f17af8982ee0bf8570b3d66
ponkiss <sm719112005@gmail.com>
2024-08-08 10:42:26 -0600

    removidos comentarios feos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,31 @@
-// Función para mostrar el modal
+
 function showModal(modalId) {
   document.getElementById(modalId).style.display = "block";
 }
 
-// Función para ocultar el modal
+
 function hideModal(modalId) {
   document.getElementById(modalId).style.display = "none";
 }
-
-// Evento para mostrar el modal de contacto cuando se haga clic en la burbuja de contacto
 document.getElementById("contact-bubble").onclick = function() {
   showModal("contact-modal");
 }
-
-// Evento para mostrar el modal de confirmación de logout cuando se haga clic en LOG OUT
 document.getElementById("logout-button").onclick = function(event) {
-  event.preventDefault(); // Previene la acción por defecto del enlace
+  event.preventDefault(); 
   showModal("logout-modal");
 }
-
-// Evento para cerrar el modal cuando se haga clic en el botón de cerrar (x)
 document.querySelectorAll(".close-button").forEach(button => {
   button.onclick = function() {
       hideModal(button.parentElement.parentElement.id);
   };
 });
 
-// Evento para cerrar el modal cuando se haga clic fuera del contenido del modal
 window.onclick = function(event) {
   if (event.target.classList.contains("contact-modal")) {
       hideModal(event.target.id);
   }
 }
 
-// Eventos para los botones de confirmación de logout
 document.getElementById("confirm-logout").onclick = function() {
   window.location.href = "/monica/pages/admin/logout/logout.php";
 }
@@ -42,7 +34,6 @@ document.getElementById("cancel-logout").onclick = function() {
   hideModal("logout-modal");
 }
 
-// Función para verificar el desplazamiento y cambiar el estilo de la burbuja de contacto
 function checkScroll() {
   const scrollTop = window.scrollY;
   const windowHeight = window.innerHeight;
@@ -56,10 +47,8 @@ function checkScroll() {
   }
 }
 
-// Agregar evento de scroll para la burbuja de contacto
 document.addEventListener('scroll', checkScroll);
 
-// Verificar el estado inicial al cargar la página
 window.addEventListener('load', function() {
   checkScroll();
 
